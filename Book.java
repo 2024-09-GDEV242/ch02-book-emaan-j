565d52358cf14fc5405fa2cdbb8e96c571cbcb3f
emaan-j <emaan.jaffer@gmail.com>
2024-09-16 15:24:45 -0400

    2.90

--- Book.java
+++ Book.java
@@ -43,7 +43,12 @@ class Book
     }
     
     public void setRefNumber(String ref){
-        refNumber = ref;
+        if (ref.length() == 3){
+            refNumber = ref;
+        }
+        else{
+            System.out.println("Error!");
+        }
     }
     
     public void printAuthor(){
