47556a7f52d695d7441dd8236beffad8e8036abf
emaan-j <emaan.jaffer@gmail.com>
2024-09-16 15:10:43 -0400

    2.85

--- Book.java
+++ Book.java
@@ -16,10 +16,11 @@ class Book
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
     
     public String getAuthor(){
@@ -28,6 +29,10 @@ class Book
 
     public String getTitle(){
         return title;
+    }
+    
+    public int getPages(){
+        return pages;
     }
     
     public void printAuthor(){
